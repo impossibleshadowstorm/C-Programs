770e54cccf1e320ba9f3c2a50440c983fdad9c0c
impossibleshado1 <72246040+impossibleshado1@users.noreply.github.com>
2020-10-02 14:54:05 +0530

    Update square.c

--- square.c
+++ square.c
@@ -2,14 +2,17 @@
 
 int main()
 {
-    int i,j,n;
+    int n;
     
+    //take input of a number
     printf("\n ENTER N::");
     scanf("%d",&n);
     
-    for(i=1;i<=n;i++)
+    //loop for rows
+    for(int i=1;i<=n;i++)
     {
-        for(j=1;j<=n;j++)
+        //loop for columns
+        for(int j=1;j<=n;j++)
         printf("$");
         printf("\n");
     }
